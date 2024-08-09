5ca2e8ac94268915061159a882782f1e7629c942
tac <tac@sotai.co>
2024-08-09 17:41:59 +0900

    #1 add bat project

---
+++ bat/src/test/java/com/example/bat/BatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
