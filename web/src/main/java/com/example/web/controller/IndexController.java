74e11064f85d0a675507d4d76e0c6f4668a446f0
tac <tac@sotai.co>
2024-08-14 10:34:34 +0900

    Make web project sample #2

---
+++ web/src/main/java/com/example/web/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
